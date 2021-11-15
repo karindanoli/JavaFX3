b2e431451667f6227c55053183f68f3053b217e4
Karin <karindanoli@gmail.com>
2021-11-15 18:18:14 -0300

    Calculadora

--- src/main/java/javafx/javafx/HelloController.java
+++ src/main/java/javafx/javafx/HelloController.java
@@ -4,13 +4,32 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class HelloController {
     @FXML
-    private Button btTest;
+    private Button btSum;
 
     @FXML
-    public void onBtTestAction() {
-        Alerts.showAlert("Alert title", "Alert header","Hello!", Alert.AlertType.INFORMATION);
-            }
+    public void onBtSumAction() {
+        try {
+            Locale.setDefault(Locale.US);
+            double height = Double.parseDouble(txtNumber1.getText());
+            double width = Double.parseDouble(txtNumber2.getText());
+            double sum = height + width;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
+        // setText vai jogar o conteúdo da variavel soma
+    }
+
+    @FXML
+    private TextField txtNumber1;
+    @FXML
+    private TextField txtNumber2;
+    @FXML
+    private Label labelResult;
 }
