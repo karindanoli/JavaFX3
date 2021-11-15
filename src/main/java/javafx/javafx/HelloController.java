b95154806e872444c26d4a184292a550213f47bf
Karin <karindanoli@gmail.com>
2021-11-15 18:41:11 -0300

    Constraints

--- src/main/java/javafx/javafx/HelloController.java
+++ src/main/java/javafx/javafx/HelloController.java
@@ -1,14 +1,19 @@
 package javafx.javafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
+    //textField: especificar o tipo de conteudo e o initializable: como vai iniciar o controller
+    //listener: controla o comportamento do controle é uma funçao que vai ser executada quando o controle sofrer alguma alteraçao ou interaçao com o usuario
     @FXML
     private Button btSum;
 
@@ -32,4 +37,13 @@ public class HelloController {
     private TextField txtNumber2;
     @FXML
     private Label labelResult;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constraints.setTextFieldDouble(txtNumber1);
+        Constraints.setTextFieldDouble(txtNumber2);//só deixa digitar double
+        Constraints.setTextFieldMaxLength(txtNumber1,12);
+        Constraints.setTextFieldMaxLength(txtNumber2,12);
+
+    }
 }
